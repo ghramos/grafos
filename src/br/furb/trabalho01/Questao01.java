14313fa1fbe897a993dd29895d51b0bb08e58c52
iwakika <iwakika@gmail.com>
2017-04-03 11:59:33 -0300

    Questão 01 inicio

--- src/br/furb/trabalho01/Questao01.java
+++ src/br/furb/trabalho01/Questao01.java
@@ -14,6 +14,117 @@ import br.furb.Utils.Grafo;
 public class Questao01 {
 	public static void main(String[] args) throws IOException {
 		
+		//Dirigido\multigrafo
+		int matriz[][] = { {0,1,0,1,1}, 
+						   {1,0,1,0,1},
+						   {0,1,0,1,1},
+						   {1,0,1,1,0},
+						   {1,0,1,1,1}
+						   };
+		//Não dirigido- Simples
+		int matriz2[][] = {{0,1,0,1,1}, 
+						   {1,0,1,0,1},
+						   {0,1,0,1,1},
+						   {1,0,1,0,0},
+						   {1,1,1,0,0}
+						   };
+		//Dirigido\Simples
+				int matriz3[][] = {{0,1,0,1,1}, 
+								   {1,0,1,0,1},
+								   {0,2,0,1,1},
+								   {1,1,1,0,0},
+								   {1,1,1,0,0}
+								   };
+		
+		
+		System.out.println(tipoDoGrafo(matriz));
+		System.out.println(tipoDoGrafo(matriz2));	
+		System.out.println(tipoDoGrafo(matriz3));
+	;	
+
+	}
+
+	public static String tipoDoGrafo(int[][] matriz) {
+		String Saida = "";
+		boolean multigrafo = false;
+		boolean simetria = true;//Simetricas ainda podem digrafos;
+		boolean regular = true;
+		boolean completo = true;
+		boolean nulo = true;
+		boolean bipartido = true;
+		
+				
+		for (int i = 0; i < matriz.length; i++) {
+			int somaLinha = 0;
+			int somaColuna = 0;
+		
+		
+			for (int j = 0; j < matriz.length; j++) {
+				//System.out.println(i+":"+j);
+				// Verifica Multigrafo
+				if (!multigrafo) {
+					if (i == j) {
+						// Verifica primeiro se está na diagonal
+						if (matriz[i][j] != 0) {
+							multigrafo = true;
+						}
+
+					} else if (matriz[i][j] > 1) {
+						// Verifica se contem arestas paralelas
+						multigrafo = true;
+					}
+				}
+				// Verifica Simetria
+				if (simetria) {
+					if (matriz[i][j] != matriz[j][i]) {
+						simetria = false;
+					}
+				}
+				//verifica Rregular
+				if(regular){
+					if( i < matriz.length -1 && j < matriz.length -1){
+						if(matriz[i][j] != matriz[i+1][j+1]){
+							regular = false;
+						}
+						
+					}
+				}
+				
+			/*	if(matrizAdjacencia[i][j] != 0){
+					somaColuna = matrizAdjacencia[i][j] ;
+				}
+				
+				*/
+				
+
+			}
+			
+		}
+		
+		if(simetria){
+			Saida = "Não dirigido";
+		}else{
+			Saida = "Dirigido";
+		}
+		if(multigrafo){
+			Saida += "- Multigrafo";
+		}else{
+			Saida += "- Simples";
+		}
+		if(regular){
+			Saida += "- Multigrafo";
+		}
+			
+		
+		return Saida;
+	}
+
+	public void arestasDoGrafo() {
+
+	}
+
+	public void grausDoVertice() {
+
 	}
 
 }
