fe577b22c5f8d9603a6f74cbaac653caa05c6cd5
siena <siena@debian.cefetsc>
2017-04-06 11:01:20 -0300

    Questao 2

--- src/br/furb/trabalho01/Questao02.java
+++ src/br/furb/trabalho01/Questao02.java
@@ -1,10 +1,44 @@
 package br.furb.trabalho01;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import br.furb.Utils.Grafo;
+import br.furb.Utils.Vertice;
+
 /*Equipe:
  * Guilherme H R
  *  Francisca E X
  */
 
 public class Questao02 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		Grafo g = new Grafo();
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		String[] str = input.readLine().split(" ");
 
+		int a = Integer.parseInt(str[0]); // Pontos-Vertices
+		int v = Integer.parseInt(str[1]); // Ligacoes-Arestas
+
+		for (int j = 0; j < v; j++) {
+
+			input = new BufferedReader(new InputStreamReader(System.in));
+			str = input.readLine().split(" ");
+
+			for (int i = 1; i <= a; i++) {// Instancia os vertices
+				g.addVertice(i, new Vertice(String.valueOf(i)));
+			}
+
+			for (int i = 0; i < v; i++) {// Faz a ligacao dos vertices
+				str = input.readLine().split(" ");
+
+				Vertice A = g.getVertice(Integer.parseInt(str[0]));
+				Vertice B = g.getVertice(Integer.parseInt(str[1]));
+
+				A.AddVerticeAdjacente(B);
+				B.AddVerticeAdjacente(A);
+			}
+		}
+	}
 }
